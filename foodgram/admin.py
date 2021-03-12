c108b4881ace6150183777c72277189da06c7f0a
Valentin Khmyrov <valkhmyrov@gmail.com>
2021-03-12 20:22:04 +0300

    Follow was added in admin

--- foodgram/admin.py
+++ foodgram/admin.py
@@ -4,7 +4,7 @@ from .models import Ingredient, QuantityOfIngredient, Recipe, Tag, Follow, Favor
 
 
 class FoodgramRecipe(admin.ModelAdmin):
-    list_display = ['title', 'author', 'text', 'time', 'pub_date', 'get_tags']
+    list_display = ['pk', 'title', 'author', 'text', 'time', 'pub_date', 'get_tags']
     search_fields = ('title',)
     list_filter = ('author',)
     empty_value_display = '-пусто-'
@@ -16,19 +16,32 @@ class FoodgramRecipe(admin.ModelAdmin):
 
 
 class FoodgramTag(admin.ModelAdmin):
-    list_display = ['title', 'checkbox_style']
+    list_display = ['pk', 'title', 'checkbox_style']
     empty_value_display = '-пусто-'
 
 
 class FoodgramIngridient(admin.ModelAdmin):
-    list_display = ['name', 'dimension']
+    list_display = ['pk', 'name', 'dimension']
     search_fields = ('name',)
 
 
 class FoodgramQuantityOfIngredient(admin.ModelAdmin):
-    list_display = ['ingredient', 'recipe', 'quantity']
+    list_display = ['pk', 'ingredient', 'recipe', 'quantity']
     search_fields = ('ingredient',)
     empty_value_display = '-пусто-'
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author_short_name', 'user_short_name')
+
+    def author_short_name(self, obj):
+        return (obj.author.get_short_name())
+
+    def user_short_name(self, obj):
+        return (obj.user.get_short_name())
+
+    author_short_name.short_description = 'Автор'
+    user_short_name.short_description = 'Автор'
 
 
 class ShopListAdmin(admin.ModelAdmin):
@@ -39,10 +52,20 @@ class ShopListAdmin(admin.ModelAdmin):
 
     user_short_name.short_description = 'Пользователь'
 
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'user_short_name', 'recipe')
+
+    def user_short_name(self, obj):
+        return (obj.user.get_short_name())
+
+    user_short_name.short_description = 'Пользователь'
+
+
 admin.site.register(Tag, FoodgramTag)
 admin.site.register(Recipe, FoodgramRecipe)
 admin.site.register(Ingredient, FoodgramIngridient)
 admin.site.register(QuantityOfIngredient, FoodgramQuantityOfIngredient)
-admin.site.register(Follow)
-admin.site.register(Favorite)
+admin.site.register(Follow, FollowAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
 admin.site.register(ShopList, ShopListAdmin)
